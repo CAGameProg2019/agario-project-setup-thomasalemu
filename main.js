d35ab86bb4d75f38e792e959a7469d449632f01f
thomasalemu <thomas.alemu@concordacademy.org>
2019-04-01 11:51:34 -0400

    TestRun
    Description

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 let canvas = document.getElementById('main');
 let c = canvas.getContext('2d');
 
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let pos = new Vector(100,100);
+let vel = new Vector(.5,.2);
+
 function init() {
-    alert("hello world!");
+    console.log(pos.toString());
     update();
 }
 
 function update() {
+    c.clearRect(0,0,canvas.width,canvas.height);
+
+    pos.addVector(vel);
+
+    c.beginPath();
+    c.arc(pos.x, pos.y, 50, 0, Math.PI*2,false);
+    c.closePath();
+    c.stroke();
 
     requestAnimationFrame(update);
 }
--- vector.js
+++ vector.js
@@ -1,7 +1,26 @@
 class Vector {
-    
-    constructor() {
+
+    constructor(x,y) {
+        this.x = x
+        this.y = y
+    }
+    addVector(vec){
+        this.x += vec.x;
+        this.y += vec.y;
+        return this;
+    }
+    //functions to write for HW
+    subVector(vec){
 
     }
+
+    scale(s){
+
+    }
+
+    toString(){
+        return '<'+ this.x+', ' + this.y+ '>';
+    }
+
 
 }
