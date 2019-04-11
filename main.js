2653940fd8bb6a9f0a6e800bf130695fbf920e6f
thomasalemu <thomas.alemu@concordacademy.org>
2019-04-11 09:06:47 -0400

    HW 4/11

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ let c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let mpos;
+
+let player;
 let foods = [] ;
 
 let colors = [
-    'blue',
-    'green',
-    'red'
+    'red',
+    '#459C42'
 ];
 
 function randomColor(){
@@ -19,31 +21,41 @@ function randomColor(){
 }
 
 function init(){
+
+    mpos= new Vector(canvas.width/2, canvas.height/2);
+
+    player = new Player(canvas.width/2, canvas.height/2,25,randomColor())
+
     for (var i=0;i<100;i++){
         let x = Math.random()* canvas.width;
         let y = Math.random()* canvas.height;
+
         foods.push(new Food(x,y,20,randomColor()));
     }
     update();
 }
 
-// function init() {
-//     let x = Math.random()* canvas.width;
-//     let y = Math.random()* canvas.height;
-//     foods.push(new Food(canvas.width/2, canvas.height/2, 20,randomColor()));
-//     update();
-// }
+function update(){
 
-function update() {
     c.clearRect(0,0,canvas.width,canvas.height);
+
 
     for (var i=0; i<foods.length;i++){
         foods[i].draw(c);
     }
 
+    player.draw(c);
+    player.x= mpos.x;
+    player.y= mpos.y;
     requestAnimationFrame(update);
 }
 
-window.addEventListener('load', function(event) {
+window.addEventListener('load', function() {
     init();
+
+    window.addEventListener('mousemove',function(event){
+        mpos.x= event.clientX-canvas.offsetLeft;
+        mpos.y= event.clientY-canvas.offsetTop;
+        console.log(mpos.toString());
+    });
 });
--- player.js
+++ player.js
@@ -1,7 +1,10 @@
-class Player {
-    
-    constructor() {
+class Player extends Food {
 
+    constructor(x,y,radius,color) {
+        super(x,y,radius,color);
+        Player.x=this.x;
+        Player.y=this.y;
     }
 
 }
+Object.assign(Player, Food);
